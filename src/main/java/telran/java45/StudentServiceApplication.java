c43af5c95281c8cbb0db96e64316df475ab7ab18
farha <farha@LAPTOP-UMOTF5D4>
2023-01-26 15:20:13 +0200

    Second step

--- src/main/java/telran/java45/StudentServiceApplication.java
+++ src/main/java/telran/java45/StudentServiceApplication.java
@@ -3,11 +3,13 @@ package telran.java45;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//GIT-token: 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
+
 @SpringBootApplication 
-public class Application {
+public class StudentServiceApplication  {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(StudentServiceApplication .class, args);
 	}
 
 }
